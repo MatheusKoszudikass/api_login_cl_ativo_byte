35add8d7817248a31bcbc2c80f17fc5138634c60
Matheus Gomes <matheusprgc@gmail.com>
2024-12-04 21:30:26 -0300

    feat: Dois metodos para verificar o token de ativação da conta é a recuperação da conta.

--- src/Interface/UserRepositoryInterface.php
+++ src/Interface/UserRepositoryInterface.php
@@ -14,6 +14,8 @@ interface UserRepositoryInterface
     public function sendWelcomeMessage(string $firstName, string $token): string; // Enviar mensagem de boas-vindas
     public function sendPasswordRecoveryMessage(string $firstName, string $token): string; // Enviar mensagem para redefinir senha
     public function validateUser(UserCreateDto $userCreateDto): ResultOperation; // Validar dados do usuário
+    public function verifyTwoTokenFactorExpired(User $user, string $email): bool; // Verificar se o token  de dois fatores expirou
+    public function verifyTokenExpiredRecoveryAccount(User $user): bool; // Verificar se o token de redefinição de senha expirou
     public function updateUser(UserResponseDto $user): ResultOperation; // Atualizar informações do usuário
     public function deleteUserById(string $id): ResultOperation; // Remover usuário por ID
     public function findUserById(string $id): ?ResultOperation; // Buscar usuário por ID
@@ -21,6 +23,5 @@ interface UserRepositoryInterface
     public function findUserByUserName(string $userName): ?ResultOperation; // Buscar usuário por nome de usuário
     public function findUserByDocument(string $document): ?ResultOperation; // Buscar usuário por CPF/CNPJ/RG
     public function authenticateUser(string $email, string $password): ResultOperation; // Autenticar usuário
-    public function initiatePasswordReset(string $email): ResultOperation; // Solicitar redefinição de senha
     public function confirmPasswordReset(string $token, string $password): ResultOperation; // Redefinir senha
 }
