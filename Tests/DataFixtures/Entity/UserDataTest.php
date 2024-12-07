4196098f528db5b915d80ebe769c9e44303f78c5
Matheus Gomes <matheusprgc@gmail.com>
2024-12-07 16:50:08 -0300

    feat: Metodo para update do usuário, e criação de um novo usuário para teste de exclusão.

--- Tests/DataFixtures/Entity/UserDataTest.php
+++ Tests/DataFixtures/Entity/UserDataTest.php
@@ -1,9 +1,6 @@
 <?php
 
 use App\Dto\Create\UserCreateDto;
-use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\User;
-
 
 class UserDataTest 
 {
@@ -11,12 +8,35 @@ class UserDataTest
     {
         $dto = new UserCreateDto();
         $dto->email = 'testuser1@example.com';
-        $dto->password ='password123'; // Utilize hashing no construtor do User.
+        $dto->password ='password123';
         $dto->firstName ='Test';
         $dto->lastName = 'User1';
         $dto->userName ='testuser1';
         $dto->cnpjCpfRg = '12345678901';
         return $dto;
+    }
 
+    public static function createUser1(): UserCreateDto
+    {
+        $dto = new UserCreateDto();
+        $dto->email = 'testuser2@example.com';
+        $dto->password ='password1234';
+        $dto->firstName ='Test';
+        $dto->lastName = 'User2';
+        $dto->userName ='testuser2';
+        $dto->cnpjCpfRg = '12345678902';
+        return $dto;
+
+    }
+
+    public static function updateUser(): UserCreateDto
+    {
+        $dto = new UserCreateDto();
+        $dto->email = 'testuser1edit@example.com';
+        $dto->firstName = 'TestEdit';
+        $dto->lastName = 'User1Edit';
+        $dto->userName ='testuser1Edit';
+        $dto->cnpjCpfRg = '12345678902';
+        return $dto;
     }
 }
