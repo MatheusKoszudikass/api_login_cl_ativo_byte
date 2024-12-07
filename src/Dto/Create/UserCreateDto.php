8658d28d8f315a229ef02d8c766a6c4be6354702
Matheus Gomes <matheusprgc@gmail.com>
2024-12-07 16:32:11 -0300

    feat: Inicialização das propriedades.

--- src/Dto/Create/UserCreateDto.php
+++ src/Dto/Create/UserCreateDto.php
@@ -2,40 +2,39 @@
 
 namespace App\Dto\Create;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Dto\BaseEntityDto;
 use Symfony\Component\Validator\Constraints as Assert;
 
-class UserCreateDto
+class UserCreateDto extends BaseEntityDto
 {
     #[Assert\NotBlank(message: "O email não pode estar vazio.")]
     #[Assert\Email(message: "O email '{{ value }}' não é um endereço de email válido.")]
     #[Assert\Length(max: 180, maxMessage: "O email não pode ter mais de {{ limit }} caracteres.")]
-    public string $email;
+    public string $email = '';
 
     #[Assert\NotBlank(message: "A senha não pode estar vazia.")]
     #[Assert\Length(min: 8, max: 20, minMessage: "A senha deve ter pelo menos {{ limit }} caracteres.", maxMessage: "A senha não pode ter mais de {{ limit }} caracteres.")]
-    public string $password;
+    public string $password = '';
 
     #[Assert\NotBlank(message: "O primeiro nome não pode estar vazio.")]
     #[Assert\Length(max: 50, maxMessage: "O primeiro nome não pode ter mais de {{ limit }} caracteres.")]
-    public string $firstName;
+    public string $firstName = '';
 
     #[Assert\NotBlank(message: "O sobrenome não pode estar vazio.")]
     #[Assert\Length(max: 50, maxMessage: "O sobrenome não pode ter mais de {{ limit }} caracteres.")]
-    public string $lastName;
+    public string $lastName = '';
 
     #[Assert\NotBlank(message: "O CNPJ/CPF/RG não pode estar vazio.")] 
-    #[Assert\Length( max: 14, maxMessage: "O CNPJ/CPF/RG não pode ter mais de {{ limit }} caracteres." )]
-    public string $cnpjCpfRg;
+    #[Assert\Length( max: 14, maxMessage: "O CNPJ/CPF/RG não pode ter mais de {{ limit }} caracteres.")]
+    public string $cnpjCpfRg = '';
 
-    public bool $legalRegister;
+    public ?bool $legalRegister = null;
 
     #[Assert\NotBlank(message: "A senha não pode estar vazia.")]
     #[Assert\Length(min: 8, max: 50, minMessage: "A senha deve ter pelo menos {{ limit }} caracteres.", maxMessage: "A senha não pode ter mais de {{ limit }} caracteres.")]
-    public string $userName;
+    public string $userName = '';
     
     public ?array $roles = [];
 
-    public string $token;
+    public string $token = '';
 }
