4b58afa0f8c9700084ebb65bc91def35fbc29c18
Matheus Gomes <matheusprgc@gmail.com>
2024-12-07 16:49:05 -0300

    refactor: Atualização no parametro do metodo updateUser, remoção do metodo authenticateUser que ficou responsavel pelo login.

--- src/Interface/UserRepositoryInterface.php
+++ src/Interface/UserRepositoryInterface.php
@@ -16,12 +16,11 @@ interface UserRepositoryInterface
     public function validateUser(UserCreateDto $userCreateDto): ResultOperation; // Validar dados do usuário
     public function verifyTwoTokenFactorExpired(User $user, string $email): bool; // Verificar se o token  de dois fatores expirou
     public function verifyTokenExpiredRecoveryAccount(User $user): bool; // Verificar se o token de redefinição de senha expirou
-    public function updateUser(UserResponseDto $user): ResultOperation; // Atualizar informações do usuário
+    public function updateUser(string $id, UserCreateDto $user): ResultOperation; // Atualizar informações do usuário
     public function deleteUserById(string $id): ResultOperation; // Remover usuário por ID
     public function findUserById(string $id): ?ResultOperation; // Buscar usuário por ID
     public function findUserByEmail(string $email): ?ResultOperation; // Buscar usuário por email
     public function findUserByUserName(string $userName): ?ResultOperation; // Buscar usuário por nome de usuário
     public function findUserByDocument(string $document): ?ResultOperation; // Buscar usuário por CPF/CNPJ/RG
-    public function authenticateUser(string $email, string $password): ResultOperation; // Autenticar usuário
     public function confirmPasswordReset(string $token, string $password): ResultOperation; // Redefinir senha
 }
