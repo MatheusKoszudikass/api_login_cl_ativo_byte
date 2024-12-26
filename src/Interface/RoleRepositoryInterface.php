7089b6d169d3253fd47fe4928d5bddb9b9e512a9
MatheusKoszudikass <matheusprgc@gmail.com>
2024-12-26 17:26:54 -0300

    feat: Metodo para verificar se role existe. Organização conforme a implementação.

--- src/Interface/RoleRepositoryInterface.php
+++ src/Interface/RoleRepositoryInterface.php
@@ -9,16 +9,12 @@ use App\Result\ResultOperation;
 
 interface RoleRepositoryInterface
 {
-
     public function createRole(RoleCreateDto $role): ResultOperation;
-    public function deleteRole(string $id): ResultOperation;
-
-    public function updateRole(RoleCreateDto $role): ResultOperation;
-
+    public function roleExists(string $name): ResultOperation;
+    public function updateRole(string $id, RoleCreateDto $role): ResultOperation;
     public function findRoleById(string $id): ?ResultOperation;
-
     public function findRoleByName(string $name): ?ResultOperation;
-
     public function findRoleAll(): ?ResultOperation;
+    public function deleteRole(string $id): ResultOperation;
 
 }
