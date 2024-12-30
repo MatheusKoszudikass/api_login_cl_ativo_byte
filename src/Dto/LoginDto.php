733af16443c1069329c9ba839866a4d1ffda575d
MatheusKoszudikass <matheusprgc@gmail.com>
2024-12-29 21:08:56 -0300

    feat: Propriedade de relembra o login.

--- src/Dto/LoginDto.php
+++ src/Dto/LoginDto.php
@@ -2,10 +2,12 @@
 
 namespace App\Dto;
 
+use phpDocumentor\Reflection\Types\Boolean;
 
 class LoginDto
 {
     public string $email_userName;
     public string $password;
+    public bool $remember;
     public string $lastLoginIp;
 }
