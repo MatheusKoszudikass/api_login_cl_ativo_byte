12ee862ae1ddea2eb0e1d4d916ac7f51d42a1966
MatheusKoszudikass <matheusprgc@gmail.com>
2025-02-03 16:02:56 -0300

    feat: Pequena mudança trazendo o valor do USER referenciando a Entidade USER.

--- src/Entity/Enum/TypeImageEnum.php
+++ src/Entity/Enum/TypeImageEnum.php
@@ -2,9 +2,12 @@
 
 namespace App\Entity\Enum;
 
+use ValueError;
+use App\Entity\User;
+
 enum TypeImageEnum: string
 {
-    case AVATAR = 'avatar';
+    case USER = User::class;
     case PRODUCT = 'product';
     case BANNER = 'banner';
     case COVER = 'cover';
