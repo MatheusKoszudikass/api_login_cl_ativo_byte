b22ff9fd73db0ae00f093b319ad93004babc3540
MatheusKoszudikass <matheusprgc@gmail.com>
2025-02-03 15:56:47 -0300

    feat: Dto responsável pelo recebimento de uma imagem fora da aplicação.

---
+++ src/Dto/Create/ImageCreateDto.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Dto\Create;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Entity\Enum\TypeImageEnum;
+use App\Dto\BaseEntityDto;
+
+class ImageCreateDto extends BaseEntityDto
+{
+    #[Assert\NotBlank]
+    #[Assert\Type('string')]
+    #[Assert\Length(max: 50, maxMessage: 'O nome da imagem deve ter no máximo 50 caracteres')]
+    public string $name = '';
+
+    #[Assert\NotBlank]
+    #[Assert\Type('string')]
+    #[Assert\Length(max: 255, maxMessage: 'O caminho da imagem deve ter no máximo 255 caracteres')]
+    public string $path = '';
+
+    #[Assert\NotBlank]
+    #[Assert\Type(TypeImageEnum::class)]
+    public TypeImageEnum $typeImage = TypeImageEnum::PRODUCT;
+
+    #[Assert\NotBlank]
+    #[Assert\Type('string')]
+    #[Assert\Length(max: 15, maxMessage: 'A extensão da imagem deve ter no máximo 15 caracteres')]
+    public string $ownerClass = '';
+
+    #[Assert\NotBlank]
+    #[Assert\Type('string')]
+    #[Assert\Length(max: 50, maxMessage: 'O ID do propriário deve ter no máximo 50 caracteres')]
+    public string $ownerId = '';
+}
+
