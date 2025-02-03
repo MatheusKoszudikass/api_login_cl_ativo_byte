f27ba7ff3297d45178156446b74045bbc245aa51
MatheusKoszudikass <matheusprgc@gmail.com>
2025-02-03 17:16:42 -0300

    feat: Implementação do repositorio imagem.

--- src/Repository/ImageRepository.php
+++ src/Repository/ImageRepository.php
@@ -3,41 +3,123 @@
 namespace App\Repository;
 
 use App\Entity\Image;
+use App\Interface\ImageRepositoryInterface;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Exception\ORMException;
 use Doctrine\Persistence\ManagerRegistry;
+use Exception;
 
-/**
- * @extends ServiceEntityRepository<Image>
- */
-class ImageRepository extends ServiceEntityRepository
+class ImageRepository extends ServiceEntityRepository implements ImageRepositoryInterface
 {
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Image::class);
     }
 
-//    /**
-//     * @return Image[] Returns an array of Image objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('i')
-//            ->andWhere('i.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('i.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+	public function create($entity): bool
+	{
+		try 
+		{
+			$this->getEntityManager()->persist($entity);
+			$this->getEntityManager()->flush();
+			return true;
+			
+		} catch (ORMException $ex) 
+		{
+			throw $ex;
+		}
+	}
 
-//    public function findOneBySomeField($value): ?Image
-//    {
-//        return $this->createQueryBuilder('i')
-//            ->andWhere('i.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+	public function delete($criteria): bool
+	{
+		try
+		{
+			$entity = self::getOneBy($criteria);
+
+			if ($entity) {
+				$this->getEntityManager()->remove($entity);
+				$this->getEntityManager()->flush();
+				return true;
+			} 
+			return false;
+
+		} catch(ORMException $ex)
+		{
+			throw $ex;
+		}
+	}
+
+	public function update($entity, $criteria)
+	{
+	
+		try 
+		{
+			$image = self::getOneBy($criteria);
+			if (!$entity) return false;
+
+			$image->setName($entity->name);
+			$image->setPath($entity->path);
+			
+			$this->getEntityManager()->persist($image);
+			$this->getEntityManager()->flush();
+
+			return $entity;
+
+		} catch(ORMException $ex)
+		{
+			throw $ex;
+		}
+	}
+
+	public function get($id): ?Image
+	{
+		try
+		{			
+			return $this->getEntityManager()->find(Image::class, $id);	
+
+		} catch(ORMException $ex)
+		{
+			throw $ex;
+		}
+	}
+
+	public function getAll(): ?array
+	{
+		try
+		{
+			return $this->getEntityManager()
+				->createQuery('SELECT i FROM App\Entity\Image i')
+				->getResult();
+
+		} catch(ORMException $ex)
+		{
+			throw $ex;
+		}
+	}
+
+	public function getOneBy($criteria): ?Image
+	{
+		try
+		{
+			return $this->getEntityManager()->
+			getRepository(Image::class)->findOneBy($criteria->toArrayParams());
+
+		} catch(ORMException $ex)
+		{
+			throw $ex;
+		}
+	}
+
+	public function getBy($criteria): ?Image
+	{
+		try
+		{
+			return $this->getEntityManager()->getRepository(Image::class)->getBy($criteria);
+
+		} catch(ORMException $ex)
+		{
+			throw $ex;
+		}
+	}
 }
